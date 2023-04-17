4e5efe11be9cedc80aedb6c86cd853b5647ec40d
Lucas Silva <terezaarual@hotmail.com>
2023-04-17 12:56:14 -0300

    atualizacao

---
+++ QA Academy - Exercicios Eazy/qaacademyTurma8/cursoAutomacaoExercicios/src/Ex02.java
@@ -0,0 +1,9 @@
+
+public class Ex02 {
+    public static void main(String[] args){
+        //2-) Faça um algoritmo para ler e exibir uma palavra (string)
+
+        String palavra = "curso";
+        System.out.println(palavra); //sysout + tab
+    }    
+}
